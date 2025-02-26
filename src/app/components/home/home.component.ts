33933d64cecf2599c709433f53d5f1307bf4c7f4
Tash112 <Phelixotieno21@gmail.com>
2025-02-26 20:12:38 +0300

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [NavbarComponent, CommonModule, RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  products  = [
+    {image: 'assets/image2.png', productName: 'orange', price: '300'},
+    {image: 'assets/image3.png', productName: 'berries', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+    {image: 'assets/image2.png', productName: 'orange', price: '1500'},
+  ]
+}
